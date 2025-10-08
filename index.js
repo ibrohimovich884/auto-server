2fff983da6f21fe8e7eb7ca7609be49afbd2903f
ibrohimovich884 <bkbekmirzayev@gmail.com>
2025-10-08 23:37:45 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import fetch from "node-fetch";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const TARGET_URL = "https://four0-mak-server-3.onrender.com/hez";
+
+setInterval(async () => {
+  try {
+    const res = await fetch(TARGET_URL);
+    console.log(`[PING] ${new Date().toLocaleTimeString()} - Status: ${res.status}`);
+  } catch (err) {
+    console.error("Xatolik:", err.message);
+  }
+}, 600000); // 10 minut = 600000 ms
+
+app.listen(3000, () => {
+  console.log("Ping server 3000-portda ishlayapti");
+});
