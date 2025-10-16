49413051611c4fca5e2808fcc33c3b288789d599
ibrohimovich884 <bkbekmirzayev@gmail.com>
2025-10-16 13:54:13 +0500

    40-mak server connected auto-server

--- index.js
+++ index.js
@@ -5,16 +5,41 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const TARGET_URL = "https://four0-mak-server-3.onrender.com/hez";
+const TARGET_URL = "http://localhost:5000/health";
 
+// Bu endpoint orqali ma'lumotni qaytaradi
+app.get("/ping-data", async (req, res) => {
+  try {
+    const response = await fetch(TARGET_URL);
+    const data = await response.json(); // u yerdan JSON qaytadi deb hisoblaymiz
+    res.json({
+      source: TARGET_URL,
+      data
+    });
+  } catch (err) {
+    res.status(500).json({ error: "Ma'lumotni olishda xatolik", detail: err.message });
+  }
+});
+
+await fetch("http://localhost:3000/notify", { 
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    message: "Health endpoint ishga tushdi!",
+    time: new Date().toISOString(),
+  }),
+});
+
+
+// Pingni 10 daqiqada bir yuborish (faqat backendni uyg‘otish uchun)
 setInterval(async () => {
   try {
     const res = await fetch(TARGET_URL);
-    console.log(`[PING] ${new Date().toLocaleTimeString()} - Status: ${res.status}`);
+    console.log(`[PING] ${new Date().toLocaleTimeString()} - Status: ${res.status}, Messenge ${res.message}`);
   } catch (err) {
-    console.error("Xatolik:", err.message);
+    console.error("Ping xatolik:", err.message);
   }
-}, 600000); // 10 minut = 600000 ms
+}, 1000); // 10 minut 600000
 
 app.listen(3000, () => {
   console.log("Ping server 3000-portda ishlayapti");
